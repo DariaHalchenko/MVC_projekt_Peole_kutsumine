4d87f64b5f59f37ad5beaeb6583a9caefbd195f9
opilane <opilane@HP-CZC2349HTC>
2025-01-09 09:58:00 +0200

    Muuta

--- MVC projekt Peole kutsumine/Controllers/HomeController.cs
+++ MVC projekt Peole kutsumine/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using MVC_projekt_Peole_kutsumine.Models;
+using System;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace MVC_projekt_Peole_kutsumine.Controllers
@@ -12,13 +11,47 @@ namespace MVC_projekt_Peole_kutsumine.Controllers
         {
             ViewBag.Message = "Ootan sind minu peole! Palun tule!!!";
             int hour = DateTime.Now.Hour;
-            ViewBag.Greeting = hour < 10 ? "Tere hommikust" : "Tere päevast";
+            ViewBag.Greeting = hour < 10 ? "Tere hommikust!" : "Tere päevast!";
             return View();
         }
+
         [HttpGet]
         public ViewResult Ankeet()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult Ankeet(Guest guest)
+        {
+            if (ModelState.IsValid)
+            {
+                E_mail(guest); // Функция для отправки письма с ответом
+                return View("Thanks", guest);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public void E_mail(Guest guest)
+        {
+            try
+            {
+                WebMail.SmtpServer = "smtp.gmail.com";
+                WebMail.SmtpPort = 587;
+                WebMail.EnableSsl = true;
+                WebMail.UserName = "daragalcenko3@gmail.com";
+                WebMail.Password = "iqer zkvm czuv lgqn";
+                WebMail.From = "daragalcenko3@gmail.com";
+                WebMail.Send("daragalcenko3@gmail.com", "Vastus kutsele", guest.Name + " vastas " + (guest.WillAttend.HasValue && guest.WillAttend.Value ? "tuleb peole" : "ei tule peole"));
+                ViewBag.Message = "Kiri on saatnud!";
+            }
+            catch (Exception)
+            {
+                ViewBag.Message = "Mul on kahju!Ei saa kirja saada!!!";
+            }
         }
     }
 }
--- MVC projekt Peole kutsumine/Models/Guest.cs
+++ MVC projekt Peole kutsumine/Models/Guest.cs
@@ -11,10 +11,10 @@ namespace MVC_projekt_Peole_kutsumine.Models
         [Required(ErrorMessage = "Sisesta nimi")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Sisesta email")]
-        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Valesti sisestatud email")]
+        [RegularExpression(@".+\@.+\..+", ErrorMessage = "Valesti sisestatud email")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Sisesta telefoni number")]
-        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Numbri alguses peal olema +372")]
+        [RegularExpression(@"\+372.+", ErrorMessage = "Numbri alguses peal olema +372")]
         public string Phone { get; set; }
         [Required(ErrorMessage = "Sisesta oma valik")]
         public bool? WillAttend { get; set; }
